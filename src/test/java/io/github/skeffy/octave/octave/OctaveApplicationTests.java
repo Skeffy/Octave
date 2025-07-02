0c3100287b3524dd72246a6b78d09dcbbcceecb7
Noah Stevens <stevens_nas@yahoo.com>
2025-07-02 17:20:04 -0400

    Initial commit

---
+++ src/test/java/io/github/skeffy/octave/octave/OctaveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.skeffy.octave.octave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
