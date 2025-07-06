560e9835a1c3c733cfb8f71afe7fe9496b32e0c8
Noah Stevens <stevens_nas@yahoo.com>
2025-07-06 11:37:44 -0400

    Initial commit

--- src/main/java/io/github/skeffy/octave/dao/LikeDao.java
+++ src/main/java/io/github/skeffy/octave/dao/LikeDao.java
@@ -1,4 +1,8 @@
 package io.github.skeffy.octave.dao;
 
 public interface LikeDao {
+
+    int addLike(int userId, int postId);
+
+    int removeLike(int userId, int postId);
 }
--- src/main/java/io/github/skeffy/octave/dao/MessageDao.java
+++ src/main/java/io/github/skeffy/octave/dao/MessageDao.java
@@ -1,4 +1,14 @@
 package io.github.skeffy.octave.dao;
 
+import io.github.skeffy.octave.model.Message;
+
 public interface MessageDao {
+
+    Message getMessages(int userId);
+
+    Message createMessage(int senderId, int recipientId, Message message);
+
+    int adminDeleteMessage(int userId, int messageId);
+
+    int deleteMessageThread(int senderId, int recipientId);
 }
