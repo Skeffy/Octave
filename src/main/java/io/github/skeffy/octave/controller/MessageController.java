5aa2f3699f71fe314cda3a174a614e90003a7c90
Noah Stevens <stevens_nas@yahoo.com>
2025-07-06 12:22:13 -0400

    Initial commit

--- src/main/java/io/github/skeffy/octave/controller/MessageController.java
+++ src/main/java/io/github/skeffy/octave/controller/MessageController.java
@@ -1,7 +1,32 @@
 package io.github.skeffy.octave.controller;
 
+import io.github.skeffy.octave.model.Message;
+import io.github.skeffy.octave.model.User;
+import io.github.skeffy.octave.dao.MessageDao;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
+@RequestMapping("/message")
 public class MessageController {
+
+    private MessageDao messageDao;
+
+    public MessageController(MessageDao messageDao) {
+        this.messageDao = messageDao;
+    }
+
+    @GetMapping
+    public Message getMessages(Principal principal){
+        return null;
+    }
+
+    @PostMapping
+    public Message sendMessage(User sender, User recipient){
+        return null;
+    }
 }
