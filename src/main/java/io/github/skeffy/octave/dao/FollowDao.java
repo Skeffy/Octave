5e2ae5d9dd2a902de88da2a49a7fb37dacb1839b
Noah Stevens <stevens_nas@yahoo.com>
2025-07-16 22:00:12 -0400

    add method signatures

--- src/main/java/io/github/skeffy/octave/dao/FollowDao.java
+++ src/main/java/io/github/skeffy/octave/dao/FollowDao.java
@@ -2,5 +2,11 @@ package io.github.skeffy.octave.dao;
 
 public interface FollowDao {
 
+    int getFollowers(int userId);
 
+    int getFollowing(int userId);
+
+    int addFollower(int followerId, int followingId);
+
+    int removeFollower(int followerId, int followingId);
 }
