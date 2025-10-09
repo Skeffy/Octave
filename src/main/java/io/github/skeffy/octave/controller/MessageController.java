b8e2d69af3f05faf0fe16eeda78284a1ddc239e0
Noah Stevens <stevens_nas@yahoo.com>
2025-10-08 21:54:57 -0400

    separate methods for getting threads and getting messages. Same for sending each

--- src/main/java/io/github/skeffy/octave/controller/MessageController.java
+++ src/main/java/io/github/skeffy/octave/controller/MessageController.java
@@ -1,14 +1,15 @@
 package io.github.skeffy.octave.controller;
 
+import io.github.skeffy.octave.exception.DaoException;
 import io.github.skeffy.octave.model.Message;
-import io.github.skeffy.octave.model.User;
 import io.github.skeffy.octave.dao.MessageDao;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.skeffy.octave.service.PrincipalService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/message")
@@ -20,13 +21,47 @@ public class MessageController {
         this.messageDao = messageDao;
     }
 
-    @GetMapping
-    public Message getMessages(Principal principal){
-        return null;
+    @GetMapping("/threads")
+    public List<Message> getThreads(Principal principal){
+        try {
+            int userId = PrincipalService.getUserId(principal);
+            return messageDao.getThreads(userId);
+        }
+        catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
     }
 
-    @PostMapping
-    public Message sendMessage(User sender, User recipient){
-        return null;
+    @GetMapping("/messages")
+    public List<Message> getMessages(Principal principal, @RequestBody int parentId) {
+        try {
+            int userId = PrincipalService.getUserId(principal);
+            return messageDao.getMessages(parentId);
+        }
+        catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/new")
+    public Message createThread(Principal principal, @RequestBody Message message) {
+        try {
+            int userId = PrincipalService.getUserId(principal);
+            return messageDao.createMessage(userId, message);
+        }
+        catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/send")
+    public Message sendMessage(Principal principal, @RequestBody Message message){
+        try {
+            int userId = PrincipalService.getUserId(principal);
+            return messageDao.addMessage(userId, message);
+        }
+        catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
     }
 }
