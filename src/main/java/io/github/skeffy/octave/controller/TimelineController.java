95be159d60ad33b34164ac937fc60027d0d51e53
Noah Stevens <stevens_nas@yahoo.com>
2025-10-08 20:55:19 -0400

    Wrap service calls in try blocks

--- src/main/java/io/github/skeffy/octave/controller/TimelineController.java
+++ src/main/java/io/github/skeffy/octave/controller/TimelineController.java
@@ -1,11 +1,14 @@
 package io.github.skeffy.octave.controller;
 
+import io.github.skeffy.octave.exception.DaoException;
 import io.github.skeffy.octave.model.Timeline;
-import io.github.skeffy.octave.dao.TimelineDao;
+import io.github.skeffy.octave.service.PrincipalService;
 import io.github.skeffy.octave.service.TimelineService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -13,22 +16,30 @@ import java.security.Principal;
 @RequestMapping("/timeline")
 public class TimelineController {
 
-    private final TimelineDao timelineDao;
     private final TimelineService timelineService;
 
-    public TimelineController(TimelineDao timelineDao, TimelineService timelineService) {
-        this.timelineDao = timelineDao;
+    public TimelineController(TimelineService timelineService) {
         this.timelineService = timelineService;
     }
 
     @GetMapping("/default")
     public Timeline getUnauthenticatedTimeline() {
-        return timelineService.createUnauthenticatedTimeline();
+        try {
+            return timelineService.createUnauthenticatedTimeline();
+        }
+        catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
     }
 
     @GetMapping("/custom")
     public Timeline getCustomUserTimeline(Principal principal) {
-        //TODO: get userId from principal
-        return timelineService.createUserSpecificTimeline(1);
+        try {
+            int userId = PrincipalService.getUserId(principal);
+            return timelineService.createUserSpecificTimeline(userId);
+        }
+        catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
     }
 }
