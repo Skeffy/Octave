582ebaf9a592103898d188121e4fc5ffdc958bc6
Noah Stevens <stevens_nas@yahoo.com>
2025-10-08 22:40:58 -0400

    Add new getPostsByAccount method

--- src/main/java/io/github/skeffy/octave/dao/PostDao.java
+++ src/main/java/io/github/skeffy/octave/dao/PostDao.java
@@ -2,11 +2,15 @@ package io.github.skeffy.octave.dao;
 
 import io.github.skeffy.octave.model.Post;
 
+import java.util.List;
+
 public interface PostDao {
 
     Post getPostById(int postId);
 
-    Post createPost(Post post);
+    List<Post> getPostsByAccount(int userId);
+
+    Post createPost(int userId, Post post);
 
     int deleteOwnPost(int userId, int postId);
 
