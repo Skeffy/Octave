575057cc43c1e407246306427f5c54ec81c8d886
Noah Stevens <stevens_nas@yahoo.com>
2025-10-08 20:54:50 -0400

    Remove try blocks. Now caught by timeline controller

--- src/main/java/io/github/skeffy/octave/service/TimelineService.java
+++ src/main/java/io/github/skeffy/octave/service/TimelineService.java
@@ -1,12 +1,9 @@
 package io.github.skeffy.octave.service;
 
-import io.github.skeffy.octave.exception.DaoException;
 import io.github.skeffy.octave.model.Post;
 import io.github.skeffy.octave.model.Timeline;
 import io.github.skeffy.octave.dao.TimelineDao;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -24,33 +21,23 @@ public class TimelineService {
     public Timeline createUnauthenticatedTimeline() {
         Timeline timeline = new Timeline();
         List<Post> posts = new ArrayList<>();
-        try {
-            posts.addAll(timelineDao.getTrendingPosts());
-            posts.addAll(timelineDao.getPostsFromPopularAccounts());
-            posts.addAll(timelineDao.getRecentPosts());
-            List<Post> sortedPosts = sortPosts(posts);
-            timeline.setPosts(sortedPosts);
-            return timeline;
-        }
-        catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
-        }
+        posts.addAll(timelineDao.getTrendingPosts());
+        posts.addAll(timelineDao.getPostsFromPopularAccounts());
+        posts.addAll(timelineDao.getRecentPosts());
+        List<Post> sortedPosts = sortPosts(posts);
+        timeline.setPosts(sortedPosts);
+        return timeline;
     }
 
     public Timeline createUserSpecificTimeline(int userId) {
         Timeline timeline = new Timeline();
         List<Post> posts = new ArrayList<>();
-        try {
-            posts.addAll(timelineDao.getFollowingPosts(userId));
-            posts.addAll(timelineDao.getPostsFromUserLikes(userId));
-            posts.addAll(timelineDao.getTrendingPosts());
-            List<Post> sortedPosts = sortPosts(posts);
-            timeline.setPosts(sortedPosts);
-            return timeline;
-        }
-        catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
-        }
+        posts.addAll(timelineDao.getFollowingPosts(userId));
+        posts.addAll(timelineDao.getPostsFromUserLikes(userId));
+        posts.addAll(timelineDao.getTrendingPosts());
+        List<Post> sortedPosts = sortPosts(posts);
+        timeline.setPosts(sortedPosts);
+        return timeline;
     }
 
     private List<Post> sortPosts(List<Post> posts) {
